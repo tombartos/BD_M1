f6daf9f88851d2451aaf660650c2497137253102
Yann Roblin <130135822+grifire@users.noreply.github.com>
2024-10-24 17:31:27 +0200

    Update query.py
    ajout du port

--- src/query.py
+++ src/query.py
@@ -3,7 +3,7 @@ import config
 
 def initConn():
     """Renvoie un objet connexion psycopg2"""
-    return psycopg2.connect( host=config.hostname, user=config.username, password=config.password, dbname=config.database )
+    return psycopg2.connect( host=config.hostname, user=config.username, password=config.password, dbname=config.database, port = config.port  )
 
 def doQuery(conn,SQL) :
     """Renvoie le résultat de la requete SQL, conn est l'objet connexion psycopg2"""
